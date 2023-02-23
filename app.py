c1c4f2835fe48575b648abddff72b06a5c198b30
Mouaaz Sweid <mmsweid@gmail.com>
2023-02-23 15:33:34 +0300

    app.py can retrieve code by handle and contestId

--- app.py
+++ app.py
@@ -2,16 +2,42 @@ import urllib.request
 import collections
 collections.Callable = collections.abc.Callable
 from bs4 import BeautifulSoup
-contestId = input("Enter the contest id: ") or "1"
-submissionId = input("Enter your submission id: ") or "165594039"
-url = "https://codeforces.com/contest/" + contestId + "/submission/" + submissionId
-print('Retrieving the code form ' + url + '\n...')
-html = urllib.request.urlopen(url).read()
-print('...')
+import json
+import sys
 
-soup = BeautifulSoup(html, "html.parser")
-for tag in soup('pre'):
-    if tag.attrs.get('id') == 'program-source-text':
-        content = tag.contents[0]
-print(content)
+def getCode(contestId, submissionId):
+    url = "https://codeforces.com/contest/" + contestId + "/submission/" + submissionId
+    print('Retrieving the code form ' + url + '\n...')
+    try:
+        html = urllib.request.urlopen(url).read()
+        print('...')
+    except:
+        print("Error in retrieval")
+        return
+    
+    soup = BeautifulSoup(html, "html.parser")
+    for tag in soup('pre'):
+      if tag.attrs.get('id') == 'program-source-text':
+          content = tag.contents[0]
+    return content
+
+
+
+contestId = input("Enter the contest id: ") or "1791"
+handle = input("Enter your handle: ") or "MEGAMMS"
+
+url = "https://codeforces.com/api/contest.status?contestId=" + contestId + "&handle=" + handle
+
+try:
+    jf = urllib.request.urlopen(url).read()
+except:
+    print("Error in retrieval")
+    sys.exit()
+
+
+info = json.loads(jf)
+for sub in info["result"]:
+    print(getCode(str(sub["contestId"]),str(sub["id"])))
+
+
         
